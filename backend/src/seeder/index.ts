b93e692b028c207cccbeb80b8f5c85c733f9182e
Isaac Annan <succianan195@gmail.com>
2026-01-23 13:27:51 +0100

    Update index.ts

--- backend/src/seeder/index.ts
+++ backend/src/seeder/index.ts
@@ -1,6 +1,7 @@
-import { createTableProduct } from './products/index.js'
+import { createTableBrand } from './brands'
+import { createTableProduct } from './products'
 
 export const ensureTableExists = async () => {
+  await createTableBrand()
   await createTableProduct()
 }
-
