87e54351186621f7cffae3a1dff79d4feba0f798
Isaac Annan <succianan195@gmail.com>
2026-01-23 23:10:21 +0100

    Update index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,6 @@
-import { defineRouter } from '#q-app/wrappers';
-import {
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from 'vue-router';
-import routes from './routes';
+import { defineRouter } from '#q-app/wrappers'
+import { createMemoryHistory, createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import routes from './routes'
 
 /*
  * If not building with SSR mode, you can
@@ -16,20 +11,20 @@ import routes from './routes';
  * with the Router instance.
  */
 
-export default defineRouter(function (/* { store, ssrContext } */) {
+export default defineRouter( function (/* { store, ssrContext } */ ) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : ( process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory )
 
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+  const Router = createRouter( {
+    scrollBehavior: () => ( { left: 0, top: 0 } ),
     routes,
 
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE),
-  });
+    history: createHistory( process.env.VUE_ROUTER_BASE ),
+  } )
 
-  return Router;
-});
+  return Router
+} )
