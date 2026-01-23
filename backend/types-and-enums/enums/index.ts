b9604c9d278274ef159b5b45f22755dadfd234d2
Isaac Annan <succianan195@gmail.com>
2026-01-23 13:28:11 +0100

    Create index.ts

---
+++ backend/types-and-enums/enums/index.ts
@@ -0,0 +1,13 @@
+export enum EnumBrandStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  PENDING = 'PENDING',
+  ARCHIVED = 'ARCHIVED',
+  BLOCKED = 'BLOCKED'
+}
+
+export enum EnumProductStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  PENDING = 'PENDING'
+}
